0fc250d3bfc2d68ea4a0024e13b79522b3f5212a
pashaie <pashaie@users.noreply.github.com>
2022-05-10 20:36:22 +0430

    components/Counter:

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 
 export default function () {
   // let cnt = 0;
-  const cntArr = useState(0);
-  const cnt = cntArr[0];
-  const setCnt = cntArr[1];
+  // const cntArr = useState(0);
+  // const cnt = cntArr[0];
+  // const setCnt = cntArr[1];
+
+  //Destructure
+  const [cnt, setCnt] = useState(0);
 
   console.log(cnt);
   return (
