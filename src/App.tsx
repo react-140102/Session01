273c34d379ed7850b308aa544b2b2ecbd5c6af94
pashaie <pashaie@users.noreply.github.com>
2022-05-10 19:26:47 +0430

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+// import "./App.css";
+
+//javascript with xml
+//1- class -> className
+//2- tag colosed
+//3- one root element
+//  fragment
+//  React.fragment  -> <></>
+//4-style -> object
 
 function App() {
+  const title = "Session 01";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{title}</h1>
+      {/* {Message("hello word!")} */}
+      <Message msg="salam"></Message>
+    </>
   );
+}
+
+//PascalCase
+function Message(props: any) {
+  const style = {
+    color: "red",
+  };
+  // return (<h3 style={{color: 'red'}} >{msg}</h3>)
+  return <h3 style={style}>{props.msg}</h3>;
+}
+
+function messageFunction(msg: string) {
+  const style = {
+    color: "red",
+  };
+  // return (<h3 style={{color: 'red'}} >{msg}</h3>)
+  return <h3 style={style}>{msg}</h3>;
 }
 
 export default App;
